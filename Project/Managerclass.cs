e59f5c07e064b004521b11771e461e3545087f0b
williamc-1 <79489576+williamc-1@users.noreply.github.com>
2021-03-26 04:11:36 +0800

    Manager class
    Create a manager class with instance variables, constructor, getters, and setters.

---
+++ Project/Managerclass.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    private string name;
+    private sex;
+    private string level;
+    private int cost;
+    
+
+    public Manager(string setLevel, int setCost, string setName, string setSex)
+    {
+        level = setLevel;
+        cost = setCost;
+        name = setName;
+        sex = setSex;
+    }
+    public Manager()
+    {
+        level = "rookie";
+        cost = 0;
+    }
+
+    public void setLevel(string resetLevel)
+    {
+        level = resetLevel;
+    }
+    public void setCost(int resetCost)
+    {
+        cost = resetCost;
+    }
+    public string getLevel()
+    {
+        return level;
+    }
+    public int getCost()
+    {
+        return cost;
+    }
+    public string getName()
+    {
+        return name;
+    }
+    public string getSex()
+    {
+        return sex;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
