27de4cbe5d9e13334f747b6dfe157c7679f7c0a9
Ana Ochoa <liquidchicken098@gmail.com>
2021-05-04 17:18:47 -0500

    Backbutton added

---
+++ Project/Menu/Assets/ToMain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToMain : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+
+}
